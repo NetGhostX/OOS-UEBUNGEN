8261aefe74037bbf3fe0bcdc9061fbd6fccc7a70
NetghostX <kelvinyawo21@gmail.com>
2025-02-08 19:33:34 +0100

    Uebungen bis 22 gemacht heute.

---
+++ uebung01/src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //aufgabe->gewüschten effekt erhalten mit typecasting
+        char c = 'c';
+        int i = c;
+        System.out.println(i);
+        long l = (long) i;
+
+        System.out.println(l);
+
+
+        System.out.println( 5==5 || 5==2);
+    }
+}
