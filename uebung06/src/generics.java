7da9967ba216344c468299557ff63d7a721f1e7b
NetghostX <kelvinyawo21@gmail.com>
2025-02-09 18:48:44 +0100

    Practiced working with lambda expression and Generics.iml

---
+++ uebung06/src/generics.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class generics {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for(int i = 0; i < 4; i++){
+            Random rand = new Random();
+            numbers.add(rand.nextInt(10));
+        }
+        System.out.println("Using for each to output numbers in console");
+        System.out.println(numbers.toString());
+        System.out.println();
+
+        numbers.forEach(System.out::println);
+
+        System.out.println("Increasing each number by 1");
+
+        numbers.forEach(number->{int increased = number +1;
+            System.out.println(increased);});
+
+    }
+}
